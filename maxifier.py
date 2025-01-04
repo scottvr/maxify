f13ad072513af97bb53f00e71859114f66a84aba
Scott VanRavenswaay <scottvr@gmail.com>
2025-01-04 02:22:29 -0600

    Update maxifier.py

--- maxifier.py
+++ maxifier.py
@@ -7,7 +7,7 @@ from urllib.parse import urljoin, urlparse
 
 # Argument parser setup
 parser = argparse.ArgumentParser(description="Recreate unminified source paths on disk from (json) .map file")
-parser.add_argument('sourcemap', nargs='?', type=argparse.FileType('r'), default='-', help="Path to (local) sourcemap file")
+parser.add_argument('sourcemap', nargs='?', type=argparse.FileType('r'), default='-', help="Path to (local) sourcemap file. (defaults to stdin)")
 parser.add_argument('-a', '--auto_map', action='store', default=None, help="URL to the .js file to auto-map")
 parser.add_argument('-o', '--out_dir', action='store', default='./output', help="Directory to save extracted files")
 parser.add_argument('-v', '--verbose', action='store_true', help="Enable verbose output")
